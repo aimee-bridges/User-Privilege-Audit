6ab2a873de55bdbf4498b657ed853cf3ad9dd9e7
aimee-bridges <a.bridges@rgu.ac.uk>
2025-08-31 16:50:55 +0100

    windows OS check

--- audit_users.py
+++ audit_users.py
@@ -11,3 +11,9 @@ import getpass
 
 #Detects the OS type
 import platform
+
+#Function to check if the script is running on a Windows OS
+def is_windows():
+    #Returns True if OS is Windows
+    return platform.system() == "Windows"
+
