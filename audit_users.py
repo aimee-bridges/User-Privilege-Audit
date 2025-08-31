a7df519b63c77e6aeccea2dc9f915b20e944999b
aimee-bridges <a.bridges@rgu.ac.uk>
2025-08-31 16:49:02 +0100

    Import require modules

---
+++ audit_users.py
@@ -0,0 +1,13 @@
+#Import required modules
+
+#Used for interacting with OS
+import os
+
+#Allows us to run system commands and capture events
+import subprocess
+
+#Retrieves the current logged in username
+import getpass
+
+#Detects the OS type
+import platform
